74712fd08d4e60f24d81202fecc0c99f8ef436ba
Angelo <Angelo@Zagarisaaa>
2018-10-29 01:04:13 +0100

    modifica VideoFinder, primi test per tag <video>

---
+++ VideoScraper/scripts/YoutubeInfoExtractor.js
@@ -0,0 +1,19 @@
+var youtubedl = require('youtube-dl');
+var url = 'https://www.youtube.com/watch?v=48Fg-j11XE0';
+// Optional arguments passed to youtube-dl.
+var options = ['--username=user', '--password=hunter2'];
+youtubedl.getInfo(url, options, function(err, info) {
+  if (err) throw err;
+ 
+  console.log('id:', info.id);
+  console.log('title:', info.title);
+  console.log('url:', info.url);
+  console.log('duration:',info._duration_hms);
+  console.log('thumbnail:', info.thumbnail);
+  console.log('description:', info.description);
+  console.log('filename:', info._filename);
+  console.log('format id:', info.format_id);
+});
+
+
+
